f7ea60938369993b1fdcd2dfb3b6d697373e251c
Mohak-787 <mohakdevkota62@gmail.com>
2026-05-18 09:05:59 +0545

    feat (user): added update user information feature

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,3 +1,4 @@
+import { validate } from "class-validator";
 import { Message } from "../../constants/message.constant";
 import { StatusCode } from "../../constants/statusCode.constant";
 import { UserService } from "../../services/user/user.service";
@@ -5,6 +6,8 @@ import { ApiError } from "../../utils/apiError";
 import { ApiResponse } from "../../utils/apiResponse";
 import { asyncHandler } from "../../utils/asyncHandler";
 import { Request, Response } from "express";
+import { plainToInstance } from "class-transformer";
+import { SignupDto } from "../../dtos/auth/signup.dto";
 
 export class UserController {
   private userService = new UserService();
@@ -22,6 +25,32 @@ export class UserController {
           result.user,
           "User information fecthed successfully"
         )
+      )
+    }
+  );
+
+  updateUser = asyncHandler(
+    async (req: Request, res: Response) => {
+      const data = plainToInstance(SignupDto, req.body);
+
+      const errors = await validate(data, {
+        groups: ["update"],
+        whitelist: true,
+        forbidNonWhitelisted: true
+      });
+
+      if (errors.length > 0) {
+        throw new ApiError(StatusCode.BAD_REQUEST, "Validation error", errors);
+      }
+
+      const result: any = await this.userService.updateUser(data, req.user);
+
+      if (result.status === StatusCode.NOT_FOUND) {
+        throw new ApiError(result.status, "User not found");
+      }
+
+      res.status(result.status).json(
+        new ApiResponse(result.status, null, "User information updated successfully")
       )
     }
   );
--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -4,6 +4,7 @@ import ServerDataSource from "../../configs/db.config";
 import { StatusCode } from "../../constants/statusCode.constant";
 import { validate } from "class-validator";
 import { plainToInstance } from "class-transformer";
+import { SignupDto } from "../../dtos/auth/signup.dto";
 
 export class UserService {
   private userRepository = ServerDataSource.getRepository(User);
@@ -29,5 +30,18 @@ export class UserService {
     }
 
     return { status: StatusCode.OK, user: info }
+  }
+
+  async updateUser(data: SignupDto, payload: any) {
+    const user = await this.userRepository.findOne({
+      where: { id: payload.id }
+    });
+
+    if (!user) {
+      return { status: StatusCode.NOT_FOUND }
+    }
+
+    await this.userRepository.update({ id: payload.id }, data);
+    return { status: StatusCode.OK }
   }
 }
