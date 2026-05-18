b0cc79a3d390539649df6f906cca3e3e77793293
Mohak-787 <mohakdevkota62@gmail.com>
2026-05-18 13:58:44 +0545

    fix (user): added user route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth/auth.route";
+import userRoutes from "./user/user.route";
 
 const router: Router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/user", userRoutes);
 
 export default router;
--- src/routes/user/user.route.ts
+++ src/routes/user/user.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
+import { UserController } from "../../controllers/user/user.controller";
 
 const router: Router = Router();
+const userController = new UserController();
 
-router.get("/info", authenticate);
-router.patch("/", authenticate);
-router.delete("/", authenticate);
+router.get("/info", authenticate, userController.userInfo);
+router.patch("/", authenticate, userController.updateUser);
+router.delete("/", authenticate, userController.deleteUser);
 
 export default Router;
